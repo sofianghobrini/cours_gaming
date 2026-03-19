51fd643fff1dbe558af96a7cdeb25495074c71bb
sofianghobrini <sofiangho31000@outlook.fr>
2026-03-19 14:18:43 +0100

    cours 10 les death zones

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+public class DeathZone : MonoBehaviour
+{
+
+    private Transform playerSpawn; // Point de respawn du joueur
+    private Animator fadeSystem; // Référence à l'Animator pour le fade
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(ReplacePlayer(collision));
+        }
+    }
+
+    private IEnumerator ReplacePlayer(Collider2D collision)
+    {
+        fadeSystem.SetTrigger("FadeIn"); // Déclenche l'animation de fade out
+        yield return new WaitForSeconds(1f); // Attendre que l'animation de fade out soit terminée
+        collision.transform.position = playerSpawn.position;
+    }
+}
